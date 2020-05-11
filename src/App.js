3780f061402750d120d2b7179487499c6e6aca43
Fery Ramadhan <feryramadhan555@gmail.com>
2020-05-11 15:57:41 +0800

    s

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"; //import class react dr React nya sndiri
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
-  );
+  )
 }
-
+/*class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hallo</h1>
+        <h2>Let's learn</h2>
+      </div>
+      //div hanya boleh 1 kali, krn dibuat per item-item
+    )
+  }
+} */
 export default App;
