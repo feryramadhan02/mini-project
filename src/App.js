36b5ebf4966f8ccbc3290a4b7929a06699a96d58
feryrmdhnc <feryramadhan555@gmail.com>
2020-05-21 16:22:14 +0800

    v

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import Login from "./components/Login";
 
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>
+      <Login />
     )
   }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,15 +5,19 @@ class Login extends React.Component {
     render() {
         return (
             <Fragment>
-                <div>
-                    <div className="wall">
-                        <div className="layout-form">
-                            <form action="/">
-                                <input type="text" name="name" placeholder="Email" required="" />
-                                <input type="password" placeholder="password" />
-                                <input type="submit" value="Login" class="btn float-right login_btn" />
-                            </form>
+                <div className="container">
+                    <div className="layout-form">
+                        <div className="layout-form__text-wrapper">
+                            <h3>Hello, Friend!</h3>
+                            <p style={{ margin: " 0 10px" }}>Enter your personal details and</p>
+                            <p style={{ margin: " 10px 20px" }}>start your journey with us</p>
+                            <button>Sign up</button>
                         </div>
+                        <form action="/">
+                            <input type="email" name="name" placeholder="Email" required="" />
+                            <input type="password" placeholder="password" />
+                            <input type="submit" value="Login" class="btn float-right login_btn" />
+                        </form>
                     </div>
                 </div>
             </Fragment>
