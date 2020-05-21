2a2ba42cc026d71fc21a8d70a5ed4eb211986ee1
feryrmdhnc <feryramadhan555@gmail.com>
2020-05-21 21:34:27 +0800

    page-login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Login from "./components/Login";
+import { BrowserRouter } from "react-router-dom";
 
 
 class App extends React.Component {
   render() {
     return (
-      <Login />
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+
     )
   }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -12,16 +12,28 @@ class Login extends React.Component {
                             <h3>Hello, Friend!</h3>
                             <p style={{ margin: " 0 10px" }}>Enter your personal details and</p>
                             <p style={{ margin: " 10px 20px" }}>start your journey with us</p>
-                            <button>Sign up</button>
+                            <button><Link to="/">Sign up</Link></button>
                         </div>
-                        <div className="layout-form__logo">
-
+                        <div className="layout-form__content">
+                            <h2>Sign in to Task Manager</h2>
+                            <ul>
+                                <li><Link to="/"><img src={require("../assets/images/Facebook_f_logo_(2019).svg")} /></Link></li>
+                                <li> <Link to="/"><img src={require("../assets/images/g+.svg")} /></Link></li>
+                                <li> <Link to="/"><img src={require("../assets/images/Linkedin.svg.png")} /></Link></li>
+                            </ul>
+                            <p className="small-text">or use your email account</p>
+                            <form action="/">
+                                <input type="email" name="name" placeholder="Email" required="" />
+                                <input type="password" placeholder="Password" />
+                                <input type="submit" value="Sign in" />
+                            </form>
+                            <div className="layout-form__content__support">
+                                <ul>
+                                    <li><Link to="/"><img src={require("../assets/images/playstore.png")} /></Link></li>
+                                    <li><Link to="/"><img src={require("../assets/images/appstore.png")} /></Link></li>
+                                </ul>
+                            </div>
                         </div>
-                        <form action="/">
-                            <input type="email" name="name" placeholder="Email" required="" />
-                            <input type="password" placeholder="password" />
-                            <input type="submit" value="Login" class="btn float-right login_btn" />
-                        </form>
                     </div>
                 </div>
             </Fragment>
